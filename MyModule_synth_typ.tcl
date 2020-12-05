af8b43436da37d06f440e883d5b2961cffc17d9f
OlgaAndreevnaGolovan <75475265+OlgaAndreevnaGolovan@users.noreply.github.com>
2020-12-05 23:13:08 +0300

    Update MyModule_synth_typ.tcl

--- MyModule_synth_typ.tcl
+++ MyModule_synth_typ.tcl
@@ -1,15 +1,15 @@
 ## Setup technology files
-include ../Source/filter/X-FAB_typ.tcl
+include ../Scripts/X-FAB_typ.tcl
 ## Setup variables
 set DESIGN Filter
 set PARAMS {}
 ## Read in Verilog HDL files
 # Top module
-read_hdl -v2001 ../Source/filter/all_model_filt/Filter.v
+read_hdl -v2001 ../Source/Filter.v
 ## Compile our code (create a technology-independent schematic)
 elaborate -parameters $PARAMS $DESIGN
 ## Setup design constraints
-read_sdc ../Source/filter/all_model_filt/Synthesis/Filter.sdc
+read_sdc ../Source/Filter_synth.sdc
 ## Synthesize our schematic (create a technology-dependent schematic)
 #synthesize -to_generic
 synthesize -to_mapped
@@ -18,8 +18,8 @@ synthesize -incremental
 report area > ../Reports/Filter_synth_area_report
 report timing > ../Reports/Filter_synth_timing_report
 ## Write out synthesized Verilog netlist
-write_hdl -mapped > ../Source/filter/all_model_filt/Synthesis/Filter_synth.v
+write_hdl -mapped > ../Source/filter/Synthesis/Filter_synth.v
 ## Write out the SDC file we will take into the place n route tool
-write_sdc > ../Source/filter/all_model_filt/Synthesis/Filter_out.sdc
+write_sdc > ../Source/Synthesis/Filter_out.sdc
 gui_show
 
